f2529b3b2c63cb26a0aeeee0dbe91cee125422ac
Udbhas Dutta <udbhasdutta@gmail.com>
2025-08-13 18:20:42 +0530

    completed frontend hero page design, made few changes to other files

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import Header from "@/components/header";
-import "./globals.css";
+import './globals.css';
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
---
+++ components/hero.jsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+
+const HeroSection = () => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100; // Adjust this value as needed
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
+  return (
+    <div className="pb-20 px-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+          Manage Your Finances <br /> with Ease
+        </h1>
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing eli, facere cum
+          quia, ad voluptate tenetur nesciunt excepturi recusandae eveniet
+          assumenda possimus.
+        </p>
+        <div className="flex justify-center space-x-4">
+          <Link href={"/dashboard"}>
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="https://google.com">
+            <Button size="lg" variant="outline" className={"px-8"}>
+              Learn More
+            </Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src={"/banner.png"}
+              width={1280}
+              height={720}
+              alt="preview image"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
