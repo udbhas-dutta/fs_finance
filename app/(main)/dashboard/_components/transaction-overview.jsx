1f2e9c267cb06b004bead8984e1649cded91900b
Udbhas Dutta <udbhasdutta@gmail.com>
2025-08-19 18:16:29 +0530

    added dashboard pie chart and arcjet middleware

--- app/(main)/dashboard/_components/transaction-overview.jsx
+++ app/(main)/dashboard/_components/transaction-overview.jsx
@@ -1,10 +1,189 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { ArrowDownRight, ArrowUpRight } from "lucide-react";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
 
-const DashboardOverview = () => {
+const COLORS = [
+  "#4E79A7", // blue
+  "#F28E2B", // orange
+  "#E15759", // red
+  "#76B7B2", // teal
+  "#59A14F", // green
+  "#EDC949", // yellow
+  "#AF7AA1", // purple
+  "#FF9DA7", // pink
+];
+
+const DashboardOverview = ({ accounts, transactions }) => {
+  const [selectedAccountId, setselectedAccountId] = useState(
+    accounts.find((a) => a.isDefault)?.id || accounts[0].id
+  );
+
+  const accountTransactions = transactions.filter(
+    (t) => t.accountId === selectedAccountId
+  );
+
+  const recentTransactions = accountTransactions
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .slice(0, 5);
+
+  const currentDate = new Date();
+  const currentMonthExpenses = accountTransactions.filter((t) => {
+    const transactionDate = new Date(t.date);
+    return (
+      t.type === "EXPENSE" &&
+      transactionDate.getMonth() === currentDate.getMonth() &&
+      transactionDate.getFullYear() === currentDate.getFullYear()
+    );
+  });
+
+  //group expenses by category
+  const expensesByCategory = currentMonthExpenses.reduce((acc, transaction) => {
+    const category = transaction.category;
+    if (!acc[category]) {
+      acc[category] = 0;
+    }
+    acc[category] += transaction.amount;
+    return acc;
+  }, {});
+
+  //format data for pie chart
+  const pieChartData = Object.entries(expensesByCategory).map(
+    ([category, amount]) => ({
+      name: category,
+      value: amount,
+    })
+  );
+  console.log(pieChartData);
+
   return (
-    <div>DashboardOverview</div>
-  )
-}
+    <div className="grid gap-4 md:grid-cols-2">
+      <Card>
+        <CardHeader
+          className={
+            "flex flex-row items-center justify-between space-y-0 pb-4"
+          }
+        >
+          <CardTitle className={"text-base font-normal"}>
+            Recent Transactions
+          </CardTitle>
+          <Select
+            value={selectedAccountId}
+            onValueChange={setselectedAccountId}
+          >
+            <SelectTrigger className="w-[140px]">
+              <SelectValue placeholder="Select Account" />
+            </SelectTrigger>
+            <SelectContent>
+              {accounts.map((account) => (
+                <SelectItem key={account.id} value={account.id}>
+                  {account.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            {recentTransactions.length === 0 ? (
+              <p className="text-center text-muted-foreground py-4">
+                No Recent Transactions
+              </p>
+            ) : (
+              recentTransactions.map((transaction) => {
+                return (
+                  <div
+                    key={transaction.id}
+                    className="flex items-center justify-between"
+                  >
+                    <div className="space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {transaction.description || "Untitled Transaction"}
+                      </p>
+                      <p className="text-sm text-muted-foreground">
+                        {format(new Date(transaction.date), "PP")}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <div
+                        className={cn(
+                          "flex items-center",
+                          transaction.type === "EXPENSE"
+                            ? "text-red-500"
+                            : "text-green-500"
+                        )}
+                      >
+                        {transaction.type === "EXPENSE" ? (
+                          <ArrowDownRight className="mr-1 h-4 w-4" />
+                        ) : (
+                          <ArrowUpRight className="mr-1 h-4 w-4" />
+                        )}
+                        ₹{transaction.amount.toFixed(2)}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Monthly Expense Breakdown</CardTitle>
+        </CardHeader>
 
-export default DashboardOverview
+        <CardContent className={"p-0 pb-5"}>
+          {pieChartData.length === 0 ? (
+            <p className="text-muted-foreground text-center py-4">
+              No expenses this month
+            </p>
+          ) : (
+            <div style={{ width: "100%", height: 300 }}>
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={pieChartData}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    dataKey={"value"}
+                    fill="none"
+                  >
+                    {pieChartData.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip formatter={(value) => `₹${value.toFixed(2)}`} />
+                  <Legend />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default DashboardOverview;
